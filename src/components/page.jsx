85c1ed6243764a12d67495e8456d94c537f2b14b
ashish_chiluka <chilukaashish@gmail.com>
2020-10-02 18:01:13 +0530

    404 not found page using React

---
+++ src/components/page.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components'
+
+import Scaredcrow from "../Scarecrow.png"
+
+import  { StyledDiv, StyledImg, Wrapper, Header, Button, Footer } from './style';
+
+
+const Page = () => {
+    return(
+        <>
+        <Header><h1>404 NOT FOUND</h1></Header>
+        <StyledDiv>
+            <StyledImg src={Scaredcrow} alt="Logo" />
+            <Wrapper>
+                <h3> I have bad news for you</h3>
+                <p>The page you are looking for might be removed or is temporarily unavailable</p>
+                <Button role="button" tabIndex="0"> Back to HomePage</Button>
+            </Wrapper>
+        </StyledDiv>
+        <Footer>
+            <p>
+            <a href="https://github.com/ashishcas">ashishcas</a>
+            @DevChallenges.io
+            </p>
+        </Footer>
+
+        </>
+    );
+
+}
+
+export default Page;
---
+++ src/components/style.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import styled from 'styled-components'
+
+
+const Footer = styled.footer`
+    display: flex;
+    justify-content: center;
+`
+const StyledDiv = styled.div`
+    display: flex;
+    margin: 0 auto;
+    align-items: space-around;
+    justify-content: center;
+    @media only screen and (max-width: 500px) {
+        flex-direction: column
+    }
+`
+const StyledImg = styled.img`
+    width: 40%;
+    @media only screen and (max-width: 500px) {
+        width: 100%;
+    }
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    h3 {
+        font-family: 'Space Mono', monospace;
+        font-color: #333333;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 64px;
+        line-height: 95px;
+        letter-spacing: -0.035em;
+    }
+    p {
+        height:108px;
+        font-color: #4F4F4F;
+        font-family: 'Space Mono', monospace;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 24px;
+        line-height: 36px;
+        letter-spacing: -0.035em
+    }
+    @media only screen and (max-width: 500px) {
+       padding: 10px;
+       h3 {
+        font-family: 'Space Mono', monospace;
+            font-style: normal;
+            font-weight: bold;
+            font-size: 48px;
+            line-height: 71px;
+            letter-spacing: -0.035em;
+       }
+    }
+`
+
+const Button = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 200px;
+    height: 30px;
+    background-color: #333333;
+    color: #fff;
+    font-family: 'Space Mono', monospace;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-size: 22px;
+    padding: 25px;
+    transition: background-color 1s;
+    @media only screen and (max-width: 500px) {
+        width: 180px;
+        height: 40px;
+    }
+    hover {
+        background-color: #fff;
+        border: 3px solid #333;
+        color: #333;
+        transition: all 0.2s;
+    }
+`
+const Header = styled.div`
+    margin: 30px;
+   h1 { 
+    font-family: Inconsolata;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 25px;
+    letter-spacing: -0.08em;
+    text-transform: uppercase;
+   }
+`
+export  { StyledImg , StyledDiv, Wrapper, Header, Button, Footer };
